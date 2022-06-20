7f70ea2a9462b8ef35f1193de379b41d5da69927
naheerF <102823570+naheerF@users.noreply.github.com>
2022-06-20 16:31:03 +0530

    Add files via upload

---
+++ RowMax1.c
@@ -0,0 +1,39 @@
+// C program to find the row with maximum number of 1s.
+#include<stdio.h>
+#include<stdbool.h>
+#define R 4
+#define C 4
+int rowWithMax1s(bool mat[R][C]) {
+	int indexOfRowWithMax1s = -1 ;
+	int maxCount = 0 ,i,j;
+	for( i = 0 ; i < R ; i++){
+		int count = 0 ;
+		for( j = 0 ; j < C ; j++ ){
+			if(mat[i][j] == 1){
+				count++ ;
+			}
+		}
+		if(count > maxCount){
+			maxCount = count ;
+			indexOfRowWithMax1s = i ;
+		}
+	}
+	
+	return indexOfRowWithMax1s ;
+}
+
+// Driver Code
+int main()
+{
+	bool mat[R][C] = { {0, 0, 0, 1},
+					{0, 1, 1, 1},
+					{1, 1, 1, 1},
+					{0, 0, 0, 0}};
+
+	int indexOfRowWithMax1s = rowWithMax1s(mat);
+	printf("Index of row with maximum 1s is %d",indexOfRowWithMax1s);
+
+	return 0;
+}
+
+
